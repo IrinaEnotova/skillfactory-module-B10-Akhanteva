f9c2e33c8cdbe76c13a79bb85be5779cee3d22f6
IrinaEnotova <irinkayashina@gmail.com>
2022-11-24 02:16:22 +0400

    feat: added more keys

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ TOKEN = '5843177964:AAHVUYtZX00lX_Bumu8IXufuyZ9WNL7buX0'
 keys = {
     'евро': 'EUR',
     'доллар': 'USD',
-    'рубль': 'RUB'
+    'рубль': 'RUB',
+    'драм': 'AMD',
+    'тенге': 'KZT'
 }
